2bc7ccb937e35d0bbe6e313cc78b39e3704920ea
gustavo-avila-itsc <gustavo10.ga23@gmail.com>
2017-11-14 20:26:05 -0300

    test factory

--- APyCON/API/tests.py
+++ APyCON/API/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from factories import UserFactory, FieldFactory
+class FieldTestCase(TestCase):
+    """Class test Field """
+    def setUp(self):
+        self.user = UserFactory()
+        self.field = FieldFactory.create(user=self.user)
+    def test_field_user(self):
+        createuser = self.field.name_user()
+        self.assertEqual(createuser, (' el usuario: ' + self.twitter + ' del campo ' + self.field.name))
 
-# Create your tests here.
+    #def test_talk(self):
+        
--- APyCON/factory.py
+++ APyCON/factory.py
@@ -7,10 +7,13 @@ class UserFactory(factory.Factory):
     class Meta:
         model = Speaker
         twiter = factory.Sequence(lambda n: '@person{0}'.format(n))
-        biografia = factory.Faker('')
-        idnum= factory.Faker()
+        biografia = factory.Sequence(lambda n: 'bigrafic person{0}'.format(n))
+        idnum= factory.Faker(@person{0})
 class FieldFactory(factory.DjangoModelFactory):
     class Meta:
         model = Field
         user = factory.SubFactory(UserFactory)
         name = factory.Sequence(lambda n: 'A_Field_Name_%d' % n)
+
+#class Talkfactory(factory.Factory):
+#	title= factory.Sequence(lambda n: 'cualquier cosa')
